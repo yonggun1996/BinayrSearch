a68fcb2744136a30b0baf44cf4a8438d7c403834
qkrdydrjs7@naver.com <qkrdydrjs7@naver.com>
2021-06-25 22:02:09 +0900

    백준 알고리즘 2110번 java 풀이입니다. 도움이 된 블로그 : https://dundung.tistory.com/54

---
+++ BinarySearch/src/InstallWIFI.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InstallWIFI {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int size = in.nextInt();
+		int count = in.nextInt();
+		
+		int[] arr = new int[size];
+		for(int i = 0; i < size; i++) {
+			arr[i] = in.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		int start = 1;
+		int end = arr[size - 1];
+		int answer = 1;
+		
+		while(start <= end) {
+			int mid = (start + end) / 2;
+			int installcount = search(arr, mid);
+			
+			if(installcount >= count) {
+				answer = Math.max(mid, answer);
+				start =  mid + 1;
+			}else {
+				end = mid - 1;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+	
+	public static int search(int[] arr, int dist) {
+		int cnt = 1;
+		int inszone = arr[0] + dist;
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] >= inszone) {
+				cnt++;
+				inszone = arr[i] + dist;
+			}
+		}
+		
+		return cnt;
+		
+	}
+
+}
